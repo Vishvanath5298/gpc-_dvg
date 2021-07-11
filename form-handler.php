4193f98e0f2609d7dcd423991596fbcae8b94504
Vishvanath Naduvinamath <vanzxc12@gmail.com>
2021-07-11 17:03:31 +0530

    .

--- form-handler.php
+++ form-handler.php
@@ -4,15 +4,15 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'vanzxc12@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
 "User Email: $visitor_email.\n".
 "Subject: $subject.\n".
 "User message: $message.\n";
 
-$to = 'avinash@gmail.com';
-$headers = "Form: $email_form \r\n";
+$to = 'viflatechnologies@gmail.com';
+$headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
